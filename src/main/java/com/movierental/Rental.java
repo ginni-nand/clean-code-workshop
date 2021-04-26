23f46064fc50bfb5e2b8b7b0ba5802a558ecde0b
ginni-nand <ginni1671.cse18@chitkara.edu.in>
2021-04-26 12:50:38 +0530

    update

--- src/main/java/com/movierental/Rental.java
+++ src/main/java/com/movierental/Rental.java
@@ -10,11 +10,42 @@ public class Rental {
     this.daysRented = daysRented;
   }
 
-  public int getDaysRented() {
+  public  int getDaysRented() {
     return daysRented;
   }
 
   public Movie getMovie() {
     return movie;
+  }
+
+
+    double amount() {
+    double thisAmount = 0;
+    switch (movie.getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (daysRented> 2)
+          thisAmount += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += daysRented * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (daysRented > 3)
+          thisAmount += (daysRented - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
+
+  int FrequentRenterPoints() {
+    int frequentRenterPoints=1;
+    if (isBonusApplicable()) frequentRenterPoints++;
+    return frequentRenterPoints;
+  }
+  private boolean isBonusApplicable()
+  {
+    return (getMovie().getPriceCode()==Movie.NEW_RELEASE) && getDaysRented()>1;
   }
 }
--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -1,9 +1,25 @@
 package com.movierental;
 
 import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
 public class CustomerTest {
     @Test
     public void test(){}
+    @Test
+    public void verifyThatTheGenratedStatementIsCorrect()
+    {
+      Customer customer=new Customer("Ginni");
+      customer.addRental(new Rental(new Movie("avengers",Movie.REGULAR),3));
+      customer.addRental(new Rental(new Movie("ruhi",Movie.CHILDRENS),2));
+      customer.addRental(new Rental(new Movie("vd",Movie.NEW_RELEASE),4));
+    assertEquals("Rental Record for Ginni\n"+
+            "\tavengers\t3.5\n"+
+            "\truhi\t1.5\n"+
+            "\tvd\t12.0\n"+
+            "Amount owed is 17.0\n"+
+            "You earned 4 frequent renter points",customer.statement());
+
+
+    }
 
 }
